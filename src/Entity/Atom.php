b301dc59271b451c1e0af3bfbdb9c4a28ea85363
Tanguy Macé <95345090+Yuyugnat@users.noreply.github.com>
2024-01-20 20:46:11 +0100

    update atoms

--- src/Entity/Atom.php
+++ src/Entity/Atom.php
@@ -22,6 +22,24 @@ class Atom
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[ORM\Column]
+    private ?float $fusionPoint = null;
+
+    #[ORM\Column]
+    private ?float $boilingPoint = null;
+
+    #[ORM\Column]
+    private ?float $atomicMass = null;
+
+    #[ORM\Column]
+    private ?float $volumicMass = null;
+
+    #[ORM\Column]
+    private ?int $discovery = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $discoverer = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +77,78 @@ class Atom
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFusionPoint(): ?float
+    {
+        return $this->fusionPoint;
+    }
+
+    public function setFusionPoint(float $fusionPoint): static
+    {
+        $this->fusionPoint = $fusionPoint;
+
+        return $this;
+    }
+
+    public function getBoilingPoint(): ?float
+    {
+        return $this->boilingPoint;
+    }
+
+    public function setBoilingPoint(float $boilingPoint): static
+    {
+        $this->boilingPoint = $boilingPoint;
+
+        return $this;
+    }
+
+    public function getAtomicMass(): ?float
+    {
+        return $this->atomicMass;
+    }
+
+    public function setAtomicMass(float $atomicMass): static
+    {
+        $this->atomicMass = $atomicMass;
+
+        return $this;
+    }
+
+    public function getVolumicMass(): ?float
+    {
+        return $this->volumicMass;
+    }
+
+    public function setVolumicMass(float $volumicMass): static
+    {
+        $this->volumicMass = $volumicMass;
+
+        return $this;
+    }
+
+    public function getDiscovery(): ?int
+    {
+        return $this->discovery;
+    }
+
+    public function setDiscovery(int $discovery): static
+    {
+        $this->discovery = $discovery;
+
+        return $this;
+    }
+
+    public function getDiscoverer(): ?string
+    {
+        return $this->discoverer;
+    }
+
+    public function setDiscoverer(string $discoverer): static
+    {
+        $this->discoverer = $discoverer;
 
         return $this;
     }
