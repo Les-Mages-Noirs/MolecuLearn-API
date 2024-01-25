4def69e5eace24c8356a118e007385ba853bf572
Rayan-guemar <94523604+Rayan-guemar@users.noreply.github.com>
2024-01-26 00:44:00 +0100

    Mise en place du bundle LexikJWTAuthenticationBundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,45 +1,68 @@
 <?php
+// api/src/Entity/User.php
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use App\State\UserPasswordHasher;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(processor: UserPasswordHasher::class, validationContext: ['groups' => ['Default', 'user:create']]),
+        new Get(),
+        new Put(processor: UserPasswordHasher::class),
+        new Patch(processor: UserPasswordHasher::class),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:create', 'user:update']],
+)]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource()]
+#[UniqueEntity('email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface {
+    #[Groups(['user:read'])]
     #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column]
-    private array $roles = [];
+    #[Assert\NotBlank]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $username = null;
 
-    /**
-     * @var string The hashed password
-     */
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column]
-    private ?string $username = null;
+    #[Assert\NotBlank(groups: ['user:create'])]
+    #[Groups(['user:create', 'user:update'])]
+    private ?string $plainPassword = null;
 
-    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Molecule::class)]
-    private Collection $molecules;
-
-    public function __construct() {
-        $this->molecules = new ArrayCollection();
-    }
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
 
     public function getId(): ?int {
         return $this->id;
@@ -49,18 +72,58 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface {
         return $this->email;
     }
 
-    public function setEmail(string $email): static {
+    public function setEmail(string $email): self {
         $this->email = $email;
 
         return $this;
     }
 
     public function getUsername(): ?string {
-        return $this->username ?? $this->email;
+        return $this->username;
     }
 
-    public function setUsername(string $username): static {
+    public function setUsername(string $username): self {
         $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): self {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array {
+        $roles = $this->roles;
+
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self {
+        $this->roles = $roles;
 
         return $this;
     }
@@ -77,65 +140,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface {
     /**
      * @see UserInterface
      */
-    public function getRoles(): array {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): static {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
     public function eraseCredentials(): void {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
-
-    /**
-     * @return Collection<int, Molecule>
-     */
-    public function getMolecules(): Collection {
-        return $this->molecules;
-    }
-
-    public function addMolecule(Molecule $molecule): static {
-        if (!$this->molecules->contains($molecule)) {
-            $this->molecules->add($molecule);
-            $molecule->setOwner($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMolecule(Molecule $molecule): static {
-        if ($this->molecules->removeElement($molecule)) {
-            // set the owning side to null (unless already changed)
-            if ($molecule->getOwner() === $this) {
-                $molecule->setOwner(null);
-            }
-        }
-
-        return $this;
+        $this->plainPassword = null;
     }
 }
